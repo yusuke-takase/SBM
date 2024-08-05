94df61c2d640eb24292994432ccd0de32ccfe067
yusuke-takase <takase_y@s.okayama-u.ac.jp>
2024-08-05 11:13:21 +0900

    first commit

---
+++ sbm/__init__.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+
+from .sbm import (
+    Field,
+    SignalFields,
+    ScanFields,
+    plot_maps
+)
+
+
+from .version import (
+    __author__,
+    __version__,
+)
+
+# levels_beam.py
+__all__ = [
+    # version.py
+    "__author__",
+    "__version__",
+    # sbm.py
+    "Field",
+    "SignalFields",
+    "ScanFields",
+    "plot_maps"
+]
---
+++ sbm/version.py
@@ -0,0 +1,4 @@
+# -*- encoding: utf-8 -*-
+
+__author__ = "Yusuke Takase"
+__version__ = "0.1.0"
