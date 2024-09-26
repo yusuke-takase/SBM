4664c27cd51068ba239a9a650f0ea6f3a7ecd4e2
yusuke-takase <takase_y@s.okayama-u.ac.jp>
2024-09-26 12:10:02 +0900

    add pipeline notebook

--- sbm/pipelines.py
+++ sbm/pipelines.py
@@ -6,6 +6,9 @@ from tqdm import tqdm
 import os
 from .main import ScanFields, DB_ROOT_PATH
 
+GREEN = '\033[92m'
+RESET = '\033[0m'
+
 class Configlation:
     def __init__(self, imo, channel):
         self.imo = imo
@@ -74,6 +77,12 @@ def sim_diff_gain_per_ch(
         channel_list=ch_info
     )
     fiducial_map = mbs.run_all()[0][config.channel]
+    cmb = mbs.generate_cmb()[0]
+    fg = mbs.generate_fg()[0]
+    input_maps = {
+        "cmb": cmb,
+        "fg": fg,
+    }
     I = fiducial_map[0]
     P = fiducial_map[1] + 1j*fiducial_map[2]
     dirpath = os.path.join(DB_ROOT_PATH, config.channel)
@@ -98,12 +107,16 @@ def sim_diff_gain_per_ch(
     if config.parallel == True:
         file_args = [(i, filename, dirpath, gain_t, gain_b, I, P, config.mdim) for i, filename in enumerate(filenames)]
         with Pool() as pool:
-            for i,result in enumerate(tqdm(pool.imap(process_gain, file_args), total=len(file_args), desc=f"Processing {config.channel}")):
+            for i, result in enumerate(tqdm(pool.imap(process_gain, file_args),
+                                            total=len(file_args),
+                                            desc=f"{GREEN}Processing {config.channel}{RESET}",
+                                            bar_format='{l_bar}{bar:10}{r_bar}',
+                                            colour='green')):
                 observed_map += result["map"]
                 noise_map += _sf.generate_noise(config.mdim, seed=syst.noise_seed)
                 sky_weight[result["xlink2"] < config.xlink_threshold] += 1.0
     else:
-        for i, filename in enumerate(tqdm(filenames, desc=f"Processing {config.channel}")):
+        for i, filename in enumerate(tqdm(filenames, desc=f"{GREEN}Processing {config.channel}{RESET}", bar_format='{l_bar}{bar:10}{r_bar}', colour='green')):
             sf = ScanFields.load_det(filename, base_path=dirpath)
             diff_gain_signal = ScanFields.diff_gain_field(gain_t[i], gain_b[i], I, P)
             output = sf.map_make(diff_gain_signal, config.mdim)
@@ -113,7 +126,7 @@ def sim_diff_gain_per_ch(
             sky_weight[xlink2 < config.xlink_threshold] += 1.0
     observed_map = np.array(observed_map)/sky_weight
     noise_map = np.array(noise_map)/sky_weight
-    return observed_map, noise_map
+    return observed_map, noise_map, input_maps
 
 def sim_diff_pointing_per_ch(
     config: Configlation,
@@ -137,6 +150,12 @@ def sim_diff_pointing_per_ch(
         channel_list=ch_info
     )
     fiducial_map = mbs.run_all()[0][config.channel]
+    cmb = mbs.generate_cmb()[0]
+    fg = mbs.generate_fg()[0]
+    input_maps = {
+        "cmb": cmb,
+        "fg": fg,
+    }
     I = fiducial_map[0]
     P = fiducial_map[1] + 1j*fiducial_map[2]
     dirpath = os.path.join(DB_ROOT_PATH, config.channel)
@@ -194,12 +213,19 @@ def sim_diff_pointing_per_ch(
             ) for i, filename in enumerate(filenames)
             ]
         with Pool() as pool:
-            for i,result in enumerate(tqdm(pool.imap(process_pointing, file_args), total=len(file_args), desc=f"Processing {config.channel}")):
+            #for i,result in enumerate(tqdm(pool.imap(process_pointing, file_args), total=len(file_args), desc=f"Processing {config.channel}")):
+            for i,result in enumerate(tqdm(pool.imap(process_pointing, file_args), total=len(file_args),
+                                           desc=f"{GREEN}Processing {config.channel}{RESET}",
+                                           bar_format='{l_bar}{bar:10}{r_bar}',
+                                           colour='green')):
                 observed_maps += result["map"]
                 sky_weight[result["xlink2"] < config.xlink_threshold] += 1.0
                 noise_map += _sf.generate_noise(config.mdim, seed=syst.noise_seed)
     else:
-        for i, filename in enumerate(tqdm(filenames, desc=f"Processing {config.channel}")):
+        #for i, filename in enumerate(tqdm(filenames, desc=f"Processing {config.channel}")):
+        for i, filename in enumerate(tqdm(filenames,
+                                          desc=f"{GREEN}Processing {config.channel}{RESET}",
+                                          bar_format='{l_bar}{bar:10}{r_bar}', colour='green')):
             sf = ScanFields.load_det(filename, base_path=dirpath)
             diff_signal = ScanFields.diff_pointing_field(rho_t[i],rho_b[i],chi_t[i],chi_b[i],P,eth_I,eth_P,o_eth_P)
             output = sf.map_make(diff_signal, config.mdim)
@@ -209,7 +235,7 @@ def sim_diff_pointing_per_ch(
             noise_map += _sf.generate_noise(config.mdim, seed=syst.noise_seed)
     observed_maps = np.array(observed_maps)/sky_weight
     noise_map = np.array(noise_map)/sky_weight
-    return observed_maps, noise_map
+    return observed_maps, noise_map, input_maps
 
 def sim_noise_per_ch(
     config: Configlation,
