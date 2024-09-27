Update main.py to fix the calculation of the h attribute in the ScanFields class

sbm/main.py
@@ -400,7 +400,7 @@ class ScanFields:
         It assume top and bottom detector make a orthogonal pair.
         """
         class_copy = copy.deepcopy(self)
-        class_copy.h *= np.exp(-1j * self.spins * (np.pi / 2))
+        class_copy.h *= np.exp(-1j * self.spins_n * (np.pi / 2))
         return class_copy
 
     def __add__(self, other):
@@ -412,7 +412,7 @@ class ScanFields:
             return NotImplemented
         result = copy.deepcopy(self)
         result.hitmap += other.hitmap
-        result.h = (self.h*self.hitmap[:, np.newaxis] + other.h*other.hitmap[:, np.newaxis])/result.hitmap[:, np.newaxis]
+        result.h = (self.h*self.hitmap[:,np.newaxis,np.newaxis] + other.h*other.hitmap[:,np.newaxis,np.newaxis])/result.hitmap[:,np.newaxis,np.newaxis]
         return result
 
     def get_coupled_field(self, signal_fields: SignalFields, spin_n_out: int, spin_m_out: int):

sbm/pipelines.py
@@ -39,7 +39,7 @@ def process_gain(args):
     result = {
         "hitmap": sf.hitmap,
         "map": output,
-        "xlink2": np.abs(sf.get_xlink(2)),
+        "xlink2": np.abs(sf.get_xlink(2,0)),
         }
     return result
 
@@ -51,7 +51,7 @@ def process_pointing(args):
     result = {
         "hitmap": sf.hitmap,
         "map": output,
-        "xlink2": np.abs(sf.get_xlink(2)),
+        "xlink2": np.abs(sf.get_xlink(2,0)),
         }
     return result
 
@@ -229,7 +229,7 @@ def sim_diff_pointing_per_ch(
             sf = ScanFields.load_det(filename, base_path=dirpath)
             diff_signal = ScanFields.diff_pointing_field(rho_t[i],rho_b[i],chi_t[i],chi_b[i],P,eth_I,eth_P,o_eth_P)
             output = sf.map_make(diff_signal, config.mdim)
-            xlink2 = np.abs(sf.get_xlink(2))
+            xlink2 = np.abs(sf.get_xlink(2,0))
             sky_weight[xlink2 < config.xlink_threshold] += 1.0
             observed_maps += output
             noise_map += _sf.generate_noise(config.mdim, seed=syst.noise_seed)