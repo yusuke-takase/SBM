9436b362de6c2c4e4fe01664ba68ac90d0ac0f47
yusuke-takase <takase_y@s.okayama-u.ac.jp>
2024-09-30 12:07:40 +0900

    add tests of abs_pointing and HWPIP

--- tests/test_sbm.py
+++ tests/test_sbm.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 from pathlib import Path
+import sbm
 from sbm import ScanFields, SignalFields, Field
 import healpy as hp
 import os
@@ -9,26 +10,26 @@ class TestSBM(unittest.TestCase):
     def setUp(self):
         print(f"Current directory: {os.getcwd()}")
         self.scan_field = ScanFields.load_det("nside_32_boresight_hwp", "tests")
-        inputmap = hp.read_map("maps/cmb_0000_nside_128_seed_33.fits", field=(0,1,2)) * 1e6
+        #inputmap = hp.read_map("maps/cmb_0000_nside_128_seed_33.fits", field=(0,1,2)) * 1e6
+        inputmap = sbm.generate_cmb(128, r=0., smb_seed=33)
         self.input_map = hp.ud_grade(inputmap, self.scan_field.nside)
         self.nside = hp.npix2nside(len(self.input_map[0]))
+        self.I = self.input_map[0]
+        self.P = self.input_map[1] + 1j*self.input_map[2]
+        dI = hp.alm2map_der1(hp.map2alm(self.input_map[0]), nside=self.nside)
+        dQ = hp.alm2map_der1(hp.map2alm(self.input_map[1]), nside=self.nside)
+        dU = hp.alm2map_der1(hp.map2alm(self.input_map[2]), nside=self.nside)
+        self.eth_I = dI[2] - 1j*dI[1]
+        self.eth_P = dQ[2] + dU[1] - 1j*(dQ[1] - dU[2])
+        self.o_eth_P = dQ[2] - dU[1] + 1j*(dQ[1] + dU[2])
 
     def test_diff_gain(self, save_output_map=False):
         g_a = 0.01
         g_b = 0.0
         delta_g = g_a - g_b
-        I = self.input_map[0]
-        P = self.input_map[1] + 1j*self.input_map[2]
 
-        print(f"delta_g: {delta_g}")
-        print(f"I.shape: {I.shape}")
-        print(f"P.shape: {P.shape}")
+        signal_field = self.scan_field.diff_gain_field(g_a, g_b, self.I, self.P)
 
-        signal_field = SignalFields(
-            Field(delta_g*I/2, spin_n=0, spin_m=0),
-            Field((2.0+g_a+g_b)*P/4, spin_n=2, spin_m=0),
-            Field((2.0+g_a+g_b)*P.conj()/4, spin_n=-2, spin_m=0),
-        )
         mdim = 2
         output_map = self.scan_field.map_make(signal_field, mdim=mdim)
         print(f"output_map.shape: {output_map.shape}")
@@ -42,51 +43,59 @@ class TestSBM(unittest.TestCase):
             self.assertTrue(np.allclose(output_map, reference))
 
     def test_diff_pointing(self, save_output_map=False):
-        I = self.input_map[0]
-        P = self.input_map[1] + 1j*self.input_map[2]
-        dI = hp.alm2map_der1(hp.map2alm(self.input_map[0]), nside=self.nside)
-        dQ = hp.alm2map_der1(hp.map2alm(self.input_map[1]), nside=self.nside)
-        dU = hp.alm2map_der1(hp.map2alm(self.input_map[2]), nside=self.nside)
-
-        eth_I = dI[2] - 1j*dI[1]
-        eth_P = dQ[2] + dU[1] - 1j*(dQ[1] - dU[2])
-        o_eth_P = dQ[2] - dU[1] + 1j*(dQ[1] + dU[2])
-
         rho_T = np.deg2rad(1/60)
         chi_T = np.deg2rad(0)
         rho_B = np.deg2rad(0)
         chi_B = np.deg2rad(0)
-        zeta   = rho_T * np.exp(1j*chi_T) - 1j*rho_B * np.exp(1j*chi_B)
-        o_zeta = rho_T * np.exp(1j*chi_T) + 1j*rho_B * np.exp(1j*chi_B) #\overline{\zeta}
 
-        spin_0_field  = Field(np.zeros(len(P)), spin_n=0, spin_m=0)
-        spin_1_field  = Field(-1.0/4.0 * (zeta*eth_I + o_zeta.conj()*o_eth_P), spin_n=1, spin_m=0)
-        spin_m1_field = spin_1_field.conj()
-        spin_2_field  = Field(P/2.0, spin_n=2, spin_m=0)
-        spin_m2_field = spin_2_field.conj()
-        spin_3_field  = Field(-1.0/4.0 * o_zeta * eth_P, spin_n=3, spin_m=0)
-        spin_m3_field = spin_3_field.conj()
+        signal_field = self.scan_field.diff_pointing_field(rho_T, chi_T, rho_B, chi_B, self.P, self.eth_I, self.eth_P, self.o_eth_P)
 
-        signal_field = SignalFields(
-            spin_0_field,
-            spin_1_field,
-            spin_m1_field,
-            spin_2_field,
-            spin_m2_field,
-            spin_3_field,
-            spin_m3_field,
-        )
         mdim = 2
         output_map = self.scan_field.map_make(signal_field, mdim=mdim)
         print(f"output_map.shape: {output_map.shape}")
 
         if save_output_map==True:
             hp.write_map("tests/reference/diff_pointing_output_map.fits", output_map, overwrite=True)
-            print("Diff gain output map is saved.")
+            print("Diff pointing output map is saved.")
         else:
             reference = hp.read_map("tests/reference/diff_pointing_output_map.fits", field=(0,1,2))
             print(f"reference.shape: {reference.shape}")
             self.assertTrue(np.allclose(output_map, reference))
+
+    def test_abs_pointing(self, save_output_map=False):
+        rho = np.deg2rad(1/60)
+        chi = np.deg2rad(0)
+
+        signal_field = self.scan_field.abs_pointing_field(rho, chi, self.I, self.P, self.eth_I, self.eth_P, self.o_eth_P)
+        mdim = 3
+        output_map = self.scan_field.map_make(signal_field, mdim=mdim)
+        print(f"output_map.shape: {output_map.shape}")
+
+        if save_output_map==True:
+            hp.write_map("tests/reference/abs_pointing_output_map.fits", output_map, overwrite=True)
+            print("Absolute pointing offset with HWP output map is saved.")
+        else:
+            reference = hp.read_map("tests/reference/abs_pointing_output_map.fits", field=(0,1,2))
+            print(f"reference.shape: {reference.shape}")
+            self.assertTrue(np.allclose(output_map, reference))
+
+    def test_hwpip_(self, save_output_map=False):
+        epsilon = 1e-5
+        phi_qi = 0.0
+        signal_field = self.scan_field.hwp_ip_field(epsilon, phi_qi, self.I)
+
+        mdim = 3
+        output_map = self.scan_field.map_make(signal_field, mdim=mdim)
+        print(f"output_map.shape: {output_map.shape}")
+
+        if save_output_map==True:
+            hp.write_map("tests/reference/hwpip_output_map.fits", output_map, overwrite=True)
+            print("HWP-IP output map is saved.")
+        else:
+            reference = hp.read_map("tests/reference/hwpip_output_map.fits", field=(0,1,2))
+            print(f"reference.shape: {reference.shape}")
+            self.assertTrue(np.allclose(output_map, reference))
+
 
     def test_noise_generation(self, save_output_map=False):
         net_ukrts = 100
@@ -94,6 +103,7 @@ class TestSBM(unittest.TestCase):
         mdim = 3
         seed = 12345
         output_map = self.scan_field.generate_noise(mdim, seed)
+
         if save_output_map==True:
             hp.write_map(f"tests/reference/noise_map_{net_ukrts}ukrts.fits", output_map, overwrite=True)
             print("Noise map is saved.")
