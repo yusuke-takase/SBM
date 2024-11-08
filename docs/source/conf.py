2cc08b036067c08370a665659e2480425fd9b7cf
yusuke-takase <takase_y@s.okayama-u.ac.jp>
2024-11-08 19:02:30 +0900

    drop pydata_sphinx_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,9 +25,9 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'sphinx.ext.intersphinx',
-    'nbsphinx',
+    #'nbsphinx',
     'sphinx.ext.autosectionlabel',
-    'pydata_sphinx_theme',
+    #'pydata_sphinx_theme',
 ]
 
 autosummary_generate = True
@@ -41,5 +41,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
-#html_theme = 'alabaster'
+#html_theme = 'pydata_sphinx_theme'
+html_theme = 'alabaster'
